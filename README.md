2c3a96763b63fadcad5d3887bf6c12b9debe60b2
Jawady Muhammad Habib <SuperTnx123@outlook.fr>
2017-06-10 12:26:38 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # meow-tool
 A tool to collect URLs from web pages (single/lists) for the purpose of information gathering ....
+
+# Setting up:
+1*  git  : git clone https://github.com/whtn12/meow-tool.git                                        
+2*  run ./setup.sh                                                                    
+3*  python __main__.py                                                
+
+# Updating:                                  
+In order to update the script or to change it's python version, use update.py script
--- update.py
+++ update.py
@@ -1,3 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+import urllib2
+import sys
+import os
 
-print "give me a bottle of rum!"
+start = "\033[1m"
+end = "\033[0;0m"
+major = sys.version_info.major
+major_str = str(major)
+
+f = open(".version")
+ver = f.read()
+print "What do you want to update ?: "
+answer =raw_input("(" + start +"S"+ end + "cript version/"+ start +"P" + end +"ython version of script) ->  ")
+print answer
+if answer == "s" or "S":
+    print "# Checking fo latest version ..."
+    ctvr = urllib2.urlopen("https://raw.githubusercontent.com/whtn12/meow-tool/master/.version").read()
+    if float(ver) == float(ctvr):
+        print "# You have the latest version! :"
+        print ver
+    else:
+        print "# If you don't have git client please exit !!"
+        os.system("git clone https://github.com/whtn12/meow-tool.git ../")
+elif answer == "p" or "P":
+    print "[*] checking you major python version ..."
+    print "[*] Major version is " + major_str
+    if major == 2:
+        print "--> You script is already compatible !!"
+    else:
+        os.system("2to3 -w __main__.py")
+else:
+    print "Please choose a valid option"
